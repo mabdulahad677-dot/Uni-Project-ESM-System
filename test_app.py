a363f8c2dfa4ecb42f247219d33b27afbe612630
awais <awais@gmail.com>
2026-05-04 15:34:30 +0500

    refined the login form

--- test_app.py
+++ test_app.py
@@ -9,9 +9,17 @@ class app(ttk.Tk):
         self.login_frame.pack(padx = 30, pady = 50)
 
         self.label = ttk.Label(self.login_frame, text = "Login", font = ("Arial", 30))
-        self.label.pack()
+        self.label.grid(row = 0, column = 0, columnspan = 2)
 
+        self.u = ttk.Label(self.login_frame, text = "Username")
+        self.u.grid(row = 1, column = 0, padx = 10, pady = 5)
+        self.ue = ttk.Entry(self.login_frame)
+        self.ue.grid(row = 1, column = 1)
 
+        self.p = ttk.Label(self.login_frame, text="Password")
+        self.p.grid(row=2, column=0, padx = 10, pady = 5)
+        self.pe = ttk.Entry(self.login_frame)
+        self.pe.grid(row=2, column=1)
 
 my_app = app()
 my_app.mainloop()
