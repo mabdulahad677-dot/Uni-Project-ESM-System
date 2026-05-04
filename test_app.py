61333f6f7d627533c9c74b4e880b45fdec43ae55
awais <awais@gmail.com>
2026-05-04 16:42:55 +0500

    version added

--- test_app.py
+++ test_app.py
@@ -4,6 +4,7 @@ class app(ttk.Tk):
     def __init__(self):
         super().__init__()
         self.title("My Test app for project")
+        self.geometry("300x400")
 
         self.login_frame = ttk.Frame(self)
         self.login_frame.pack(padx = 30, pady = 50)
@@ -20,6 +21,9 @@ class app(ttk.Tk):
         self.p.grid(row=2, column=0, padx = 10, pady = 5)
         self.pe = ttk.Entry(self.login_frame)
         self.pe.grid(row=2, column=1)
+
+        self.ver = ttk.Label(self.login_frame, text = "ver 1.0.0")
+        self.ver.grid(row = 3, column = 0, columnspan = 2)
 
 my_app = app()
 my_app.mainloop()
